773bb77d64b1febfa256e45e02c6e3043dcdf41f
Zhenya Nemerovchenko <nemerov4enko@gmail.com>
2018-02-08 23:57:22 +0200

    Loading data into state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -4,27 +4,6 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.addFish = this.addFish.bind(this);
-    // getinitialState
-    this.state = {
-      fishes: {},
-      order: {}
-    };
-  }
-
-  addFish(fish) {
-    // update our state
-    const fishes = {...this.state.fishes};
-    // add in our new fish
-    const timestamp = Date.now();
-    fishes[`fish-${timestamp}`] = fish;
-    // set state
-    this.setState({ fishes });
-  }
-
   render() {
     return (
       <div className="catch-of-the-day">
@@ -32,7 +11,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory />
       </div>
     )
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Inventory</h2>
-        <AddFishForm addFish={this.props.addFish}/>
-      </div>
+      <p>Inventory</p>
     )
   }
 }
